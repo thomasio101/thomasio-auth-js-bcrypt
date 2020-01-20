1d3b82b378bfa43a21596fb9847e23b178a1af15
Thomas Egbert Duursma <thomasduursma@outlook.com>
2020-01-20 19:20:41 +0100

    1.1.0

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import { Processor, Verifier } from 'thomasio-auth-js-common/lib/server';
 export const verifier: Verifier<string> = (stored, input) => compare(input, stored);
 
 export function getProcessor(cost: number): Processor<string> {
-    return (input) => hash(input, cost)
+	return input => hash(input, cost);
 }
