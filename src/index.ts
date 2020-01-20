763eac35dfb54c9241ce70309c9c417a5378eef8
Thomas Egbert Duursma <thomasduursma@outlook.com>
2020-01-20 19:19:39 +0100

    Remove unnecessary async/await.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { compare } from 'bcryptjs';
 import { Verifier } from 'thomasio-auth-js-common/lib/server';
 
-export const verifier: Verifier<string> = async (stored, input) => await compare(input, stored);
+export const verifier: Verifier<string> = (stored, input) => compare(input, stored);
